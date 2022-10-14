2edf97cc69b6358fa99ccee3f097f52af3544d2b
alunoctg <alunoctg@fiern.local>
2022-10-14 08:14:06 -0300

    Novo commit

--- lovecatalogo/settings.py
+++ lovecatalogo/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'lovecatalogo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'catalogo',
         'USER': 'root',
         'PASSWORD': 'Senai2019,',
